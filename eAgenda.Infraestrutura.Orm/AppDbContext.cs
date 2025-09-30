335920c283aaac0c617055188a0b22552880211e
Marlon Quilante <marlonquilante@gmail.com>
2025-09-30 19:28:07 -0300

    Add project files.

---
+++ eAgenda.Infraestrutura.Orm/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eAgenda.Infraestrutura.Orm
+{
+    public class AppDbContext : DbContext
+    {
+
+    }
+}
---
+++ eAgenda.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eAgenda.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

ada24abd85454880801b2452ffb7248a05d34f7f
Marlon Quilante <marlonquilante@gmail.com>
2025-10-16 19:59:55 -0300

    Implementado os testes de Edição e Exclusão de contato

--- eAgenda.Testes.Integracao/ModuloContato/RepositorioContatoTestes.cs
+++ eAgenda.Testes.Integracao/ModuloContato/RepositorioContatoTestes.cs
@@ -35,5 +35,43 @@ namespace eAgenda.Testes.Integracao.ModuloContato
             // Asserção
             Assert.AreNotEqual(contato, contatoSelecionado);
         }
+
+        [TestMethod]
+        public void Deve_EditarRegistro_ComSucesso()
+        {
+            // Padrão AAA
+
+            // Arranjo
+            Contato contatoOriginal = new Contato("Teste", "(49) 99999-9999", "teste@teste.com", "Empresa Teste", "Cargo Teste");
+
+            repositorioContato.Cadastrar(contatoOriginal);
+
+            Contato contatoEditado = new Contato("Teste Editado", "(49) 11111-1111", "testeeditado@teste.com", "Empresa Teste Editada", "Cargo Teste Editado");
+            contatoEditado.Id = contatoOriginal.Id;
+
+            // Ação
+            repositorioContato.Editar(contatoOriginal.Id, contatoEditado);
+
+            // Asserção
+            Contato? contatoSelecionado = repositorioContato.SelecionarRegistroPorId(contatoOriginal.Id);
+
+            Assert.AreEqual(contatoOriginal, contatoSelecionado);
+        }
+
+        [TestMethod]
+        public void Deve_ExcluirRegistro_ComSucesso()
+        {
+            // Arranjo
+            Contato contatoOriginal = new Contato("Teste", "(49) 99999-9999", "teste@teste.com", "Empresa Teste", "Cargo Teste");
+
+            repositorioContato.Cadastrar(contatoOriginal);
+            // Ação
+            repositorioContato.Excluir(contatoOriginal.Id);
+
+            // Asserção
+            Contato? contatoSelecionado = repositorioContato.SelecionarRegistroPorId(contatoOriginal.Id);
+
+            Assert.IsNull(contatoSelecionado);
+        }
     }
 }

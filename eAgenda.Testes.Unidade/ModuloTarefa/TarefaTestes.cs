6b947a7bcaff7d37ffb27ceb8b486bd799d4a987
Marlon Quilante <marlonquilante@gmail.com>
2025-11-08 19:29:36 -0300

    Finalizado a implementação de testes de unidade do módulo de tarefa

--- eAgenda.Testes.Unidade/ModuloTarefa/TarefaTestes.cs
+++ eAgenda.Testes.Unidade/ModuloTarefa/TarefaTestes.cs
@@ -34,5 +34,53 @@ namespace eAgenda.Testes.Unidade.ModuloTarefa
             Assert.IsFalse(tarefa.StatusConclusao);
             Assert.IsNull(tarefa.DataConclusao);
         }
+
+        [TestMethod]
+        public void Deve_AdicionarItem_ComSucesso()
+        {
+            // Arranjo
+            Tarefa tarefa = new Tarefa("Tarefa teste", PrioridadeTarefa.Normal);
+            ItemTarefa item = new ItemTarefa("Item teste", tarefa);
+
+            // Ação
+            tarefa.AdicionarItem(item);
+
+            // Asserção
+            Assert.IsTrue(tarefa.ItensTarefa.Any());
+        }
+
+        [TestMethod]
+        public void Deve_RemoverItem_ComSucesso()
+        {
+            // Arranjo
+            Tarefa tarefa = new Tarefa("Tarefa teste", PrioridadeTarefa.Normal);
+            ItemTarefa item = new ItemTarefa("Item teste", tarefa);
+            tarefa.AdicionarItem(item);
+
+            // Ação
+            tarefa.RemoverItem(item);
+
+            // Asserção
+            Assert.IsFalse(tarefa.ItensTarefa.Any());
+        }
+
+        [TestMethod]
+        public void Deve_AtualizarPercentualConclusao_ComSucesso()
+        {
+            // Arranjo
+            Tarefa tarefa = new Tarefa("Tarefa teste", PrioridadeTarefa.Normal);
+            ItemTarefa item1 = new ItemTarefa("Item teste 1", tarefa);
+            ItemTarefa item2 = new ItemTarefa("Item teste 2", tarefa);
+
+            tarefa.AdicionarItem(item1);
+            tarefa.AdicionarItem(item2);
+            item1.MarcarConcluido();
+
+            // Ação
+            tarefa.AtualizarPercentualConclusao();
+
+            // Asserção
+            Assert.IsTrue(tarefa.PercentualConclusao == 50);
+        }
     }
 }

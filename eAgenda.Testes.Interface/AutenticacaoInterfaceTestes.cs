1d50bae7f770b5c3d68f67de3631bfb6ad3af5f6
Marlon Quilante <marlonquilante@gmail.com>
2025-11-13 20:47:11 -0300

    Feito algumas melhorias nos testes de interface da autenticação de usuário

--- eAgenda.Testes.Interface/AutenticacaoInterfaceTestes.cs
+++ eAgenda.Testes.Interface/AutenticacaoInterfaceTestes.cs
@@ -1,50 +1,49 @@
-﻿using OpenQA.Selenium;
+﻿using eAgenda.Testes.Interface.Compartilhado;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace eAgenda.Testes.Interface
 {
     [TestClass]
     [TestCategory("Testes de Interface de Autenticação")]
-    public sealed class AutenticacaoInterfaceTestes
+    public sealed class AutenticacaoInterfaceTestes : TestFixture
     {
-        private static WebDriver webDriver;
-        private string enderecoBase = "https://localhost:9001";
-
-        [AssemblyCleanup]
-        public static void LimparAmbiente()
-        {
-            if (webDriver == null)
-                return;
-
-            webDriver.Quit();
-            webDriver.Dispose();
-        }
-
         [TestMethod]
         public void Deve_RegistrarUsuario_ComSucesso()
         {
-            webDriver = new ChromeDriver();
-
             webDriver.Navigate().GoToUrl(Path.Combine(enderecoBase, "Autenticacao", "Registro"));
 
             webDriver.FindElement(By.CssSelector("input[data-se=inputEmail]")).SendKeys("teste@gmail.com");
-            webDriver.FindElement(By.CssSelector("input[data-se=inputSenha]")).SendKeys("Teste@123");
-            webDriver.FindElement(By.CssSelector("input[data-se=inputSenhaConfirmada]")).SendKeys("Teste@123");
+            webDriver.FindElement(By.CssSelector("input[data-se=inputSenha]")).SendKeys("MarlonQuilante@123");
+            webDriver.FindElement(By.CssSelector("input[data-se=inputSenhaConfirmada]")).SendKeys("MarlonQuilante@123");
 
             webDriver.FindElement(By.CssSelector("button[data-se=btnConfirmar]")).Click();
+
+            Assert.IsTrue(webDriver.PageSource.Contains("Página Inicial"));
         }
 
         [TestMethod]
         public void Deve_AutenticarUsuario_ComSucesso()
         {
-            webDriver = new ChromeDriver();
-
-            webDriver.Navigate().GoToUrl(Path.Combine(enderecoBase, "Autenticacao", "Login"));
+            // Arranjo
+            webDriver.Navigate().GoToUrl(Path.Combine(enderecoBase, "Autenticacao", "Registro"));
 
             webDriver.FindElement(By.CssSelector("input[data-se=inputEmail]")).SendKeys("teste@gmail.com");
-            webDriver.FindElement(By.CssSelector("input[data-se=inputSenha]")).SendKeys("Teste@123");
+            webDriver.FindElement(By.CssSelector("input[data-se=inputSenha]")).SendKeys("MarlonQuilante@123");
+            webDriver.FindElement(By.CssSelector("input[data-se=inputSenhaConfirmada]")).SendKeys("MarlonQuilante@123");
+
+            webDriver.FindElement(By.CssSelector("button[data-se=btnConfirmar]")).Click();
+
+            webDriver.FindElement(By.CssSelector("button[data-se=btnSair]")).Click();
+
+            // Ação
+            webDriver.FindElement(By.CssSelector("input[data-se=inputEmail]")).SendKeys("teste@gmail.com");
+            webDriver.FindElement(By.CssSelector("input[data-se=inputSenha]")).SendKeys("MarlonQuilante@123");
 
             webDriver.FindElement(By.CssSelector("button[data-se=btnEntrar]")).Click();
+
+            // Asserção
+            Assert.IsTrue(webDriver.PageSource.Contains("Página Inicial"));
         }
     }
 }
---
+++ eAgenda.Testes.Interface/Compartilhado/TestFixture.cs
@@ -0,0 +1,57 @@
+﻿using eAgenda.Infraestrutura.Orm;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace eAgenda.Testes.Interface.Compartilhado
+{
+    [TestClass]
+    public abstract class TestFixture
+    {
+        protected static WebDriver? webDriver;
+        protected static AppDbContext? dbContext;
+        protected string enderecoBase = "https://localhost:9001";
+
+        [AssemblyInitialize]
+        public static void InicializarTestFixture(TestContext testContext)
+        {
+            dbContext = AppDbContextFactory.CriarDbContext("Data source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=eAgendaTest;Integrated Security=True;");
+
+            webDriver = new ChromeDriver();
+        }
+        
+        [AssemblyCleanup]
+        public static void LimparAmbiente()
+        {
+            if (webDriver == null)
+                return;
+
+            webDriver.Quit();
+            webDriver.Dispose();
+        }
+
+        [TestInitialize]
+        public void InicializarTest()
+        {
+            if (dbContext is null || webDriver is null)
+                return;
+
+            dbContext.Database.EnsureCreated();
+
+            dbContext.Tarefas.RemoveRange(dbContext.Tarefas);
+            dbContext.Despesas.RemoveRange(dbContext.Despesas);
+            dbContext.Categorias.RemoveRange(dbContext.Categorias);
+            dbContext.Compromissos.RemoveRange(dbContext.Compromissos);
+            dbContext.Contatos.RemoveRange(dbContext.Contatos);
+
+            dbContext.UserClaims.RemoveRange(dbContext.UserClaims);
+            dbContext.UserTokens.RemoveRange(dbContext.UserTokens);
+            dbContext.UserRoles.RemoveRange(dbContext.UserRoles);
+            dbContext.UserLogins.RemoveRange(dbContext.UserLogins);
+            dbContext.Users.RemoveRange(dbContext.Users);
+
+            dbContext.SaveChanges();
+
+            webDriver.Manage().Cookies.DeleteAllCookies();
+        }
+    }
+}
